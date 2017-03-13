5cf7bcc0ac2b05b0914c8983669a041ba5131276
Mark Pollack <mpollack@pivotal.io>
2017-03-13 13:18:36 -0700

    initial

---
+++ src/main/java/io/spring/stream/MetricsdemoApplication.java
@@ -0,0 +1,12 @@
+package io.spring.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsdemoApplication.class, args);
+	}
+}
